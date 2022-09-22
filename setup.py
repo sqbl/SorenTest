2b35d7e52a41323de44a53e2a0c3273b93cc9409
Søren <sorenbertelsen@gmail.com>
2022-09-22 20:59:28 +0200

    second commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+setup(
+  name = 'sorentest',         # How you named your package folder (MyLib)
+  packages = ['sorentest'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'A package to test pypi, github actions and other automations',   # Give a short description about your library
+  author = 'Søren Bertelsen',                   # Type in your name
+  author_email = 'sorenbertelsen@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/sqbl/SorenTest',   # Provide either the link to your github or to your website
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'numpy',
+          
+      ],
+  )
