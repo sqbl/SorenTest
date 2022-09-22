1198ffb8524f260e93098515b4b00f8071cfbdd3
Søren <sorenbertelsen@gmail.com>
2022-09-22 21:06:05 +0200

    commit 4

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
